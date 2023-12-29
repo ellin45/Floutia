3950d4d5298c78d8d3814382f79dfbb130c1352c
정채림 <chaelimj45@gmail.com>
2023-12-29 18:20:38 +0900

    "feat: spotify 로그인  중 login redirect까지 구현"

--- floutia/src/app/(beforeLogin)/components/login.tsx
+++ floutia/src/app/(beforeLogin)/components/login.tsx
@@ -3,6 +3,7 @@ import React, {ComponentType} from "react";
 import {randomBytes, createHash} from "crypto";
 import qs from "querystring";
 
+
 interface RandomStringOptions {
   length: number;
 }
@@ -24,18 +25,10 @@ export default async function login(req: NextApiRequest, res: NextApiResponse) {
   };
   const codeVerifier = generateRandomString({length: 64});
 
-  const sha256 = async (input: Sha256Input): Promise<ArrayBuffer> => {
-    // window 객체가 존재하는지 확인
-    if (typeof window !== "undefined") {
-      const encoder = new TextEncoder();
-      const data = encoder.encode(input.plain);
-      return window.crypto.subtle.digest("SHA-256", data);
-    } else {
-      // window 객체가 없을 때의 대체 로직
-      // 예: 서버 사이드에서 Node.js의 crypto 모듈을 사용
-      throw new Error("window is not defined");
-    }
+  const sha256 = (input: Sha256Input): Buffer => {
+    return createHash("sha256").update(input.plain).digest();
   };
+
   const base64encode = (input: Base64Input): string => {
     const uint8Array = new Uint8Array(input.buffer);
     const charCodeArray = Array.from(uint8Array, (byte) =>
