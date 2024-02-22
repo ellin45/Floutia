360d34abd03b415d53398660d4de2e4c91d96f17
정채림 <chaelimj45@gmail.com>
2024-02-22 23:23:44 +0900

    "feat: 오늘 배운것 정리"

--- floutia/src/hooks/mutations/me.ts
+++ floutia/src/hooks/mutations/me.ts
@@ -19,3 +19,7 @@ export const usePostPlaybackQueue = () => {
 
   return {mutate};
 };
+
+//모든 타입은 직접 경로를 타고 들어가 에러코드를 보고 해결하기 export가 안되있는 경우가 있으니
+//interface는 export interface로 변경해 주고 {}로 닫기
+//type은 export declare type으로 선언하기
