eead83c269f4cd1fda7b0a06e95498bac0cbb0a0
정채림 <chaelimj45@gmail.com>
2024-02-22 23:28:54 +0900

    "feat: uri.track 타입 수정"

--- floutia/src/components/common/PlaylistTrack/index.tsx
+++ floutia/src/components/common/PlaylistTrack/index.tsx
@@ -1,47 +1,47 @@
 import React, {useState} from "react";
 import styles from "../../../styles/page.module.css";
 import Image from "next/image";
-import { useRecoilValue } from 'recoil';
-import { usePostPlaybackQueue } from 'hooks/mutations/me';
-import { loginDataState } from 'recoil/atoms';
+import {useRecoilValue} from "recoil";
+import {usePostPlaybackQueue} from "hooks/mutations/me";
+import {loginDataState} from "recoil/atoms";
 
 interface PlaylistTrackProps {
   track: SpotifyApi.TrackObjectFull;
-}
-
-interface Uri {
   uri: string;
 }
 
-const PlaylistTrack = ({ track }: PlaylistTrackProps) => {
+const PlaylistTrack = ({track}: PlaylistTrackProps) => {
   const loginData = useRecoilValue(loginDataState);
-  const { mutate } = usePostPlaybackQueue(); // Pass Uri as a generic parameter
+  const {mutate} = usePostPlaybackQueue(); // Pass Uri as a generic parameter
 
   const handlePlay = () => {
-    if (!loginData) return alert('로그인이 필요합니다.');
+    if (!loginData) return alert("로그인이 필요합니다.");
 
-    mutate({ uri: track.uri }); // Correct the syntax when calling mutate
+    mutate({uri: track.uri}); // Correct the syntax when calling mutate
   };
 
   return (
     <div className={styles.playList}>
-       <div
-       
-      >
+      <div>
         <Image
           src={track.album.images[2].url}
           alt={track.name}
           width={64}
           height={64}
-         
         />
         <div>
           <title>{track.name}</title>
-          <div>{track.artists.map((artist) => artist.name).join(', ')}</div>
+          <div>{track.artists.map((artist) => artist.name).join(", ")}</div>
         </div>
       </div>
       <div>{track.album.name}</div>
-      <Image src="/images/play.svg" alt="artist" width={24} height={24} onClick={handlePlay} />
+      <Image
+        src="/images/play.svg"
+        alt="artist"
+        width={24}
+        height={24}
+        onClick={handlePlay}
+      />
     </div>
   );
 };
