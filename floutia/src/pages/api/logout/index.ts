2cb4076fe50c2739d112b0a503de11a82bf335ac
정채림 <chaelimj45@gmail.com>
2024-04-26 07:58:13 +0900

    "feat: 새로운 버젼 2.0.0"

--- floutia/src/pages/api/logout/index.ts
+++
@@ -1,8 +0,0 @@
-import { deleteCookie } from 'cookies-next';
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  deleteCookie('refresh_token', { req, res });
-  deleteCookie('access_token', { req, res });
-  res.redirect('/');
-}
